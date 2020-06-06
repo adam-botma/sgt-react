3b71f1d58fe0fccf7317c0909dd9784f5da986e0
Adam Botma <adam.botma@me.com>
2020-06-06 00:53:52 +0000

    Add Header Component - Add Start to Grade Table Component - Add App Contructor Function with initial state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Header from './header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
   render() {
-    return null;
+    return (
+      <div className='container'>
+        <header className='row'>
+          <div className='col col-9'>
+            <Header/>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
