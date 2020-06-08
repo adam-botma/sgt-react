75e793cdeb85f982aaca7dc4b4179edb1bb25b8e
Adam Botma <adam.botma@me.com>
2020-06-08 20:34:20 +0000

    User Can Add a Grade

--- client/components/GradeForm.jsx
+++ client/components/GradeForm.jsx
@@ -39,19 +39,19 @@ class GradeForm extends React.Component {
         <div className="form-group row">
           <label htmlFor="name" className='col-sm-2 col-form-label'><i className='fas fa-user'></i></label>
           <div className='col-sm-10 input'>
-            <input type="text" name="name" id="name" className = 'w-100'placeholder={this.state.name} onChange = {this.handleChange}/>
+            <input type="text" name="name" id="name" className = 'w-100'value={this.state.name} onChange = {this.handleChange}/>
           </div>
         </div>
         <div className='form-group row'>
           <label htmlFor="course" className='col-sm-2 col-form-label'><i className='far fa-window-maximize'></i></label>
           <div className='col-sm-10 input'>
-            <input type="text" name="course" id="course" className="w-100" placeholder={this.state.course} onChange = {this.handleChange}/>
+            <input type="text" name="course" id="course" className="w-100" value={this.state.course} onChange = {this.handleChange}/>
           </div>
         </div>
         <div className='form-group row'>
           <label htmlFor="grade" className='col-sm-2 col-form-label'><i className='fas fa-graduation-cap'></i></label>
           <div className='col-sm-10 input'>
-            <input type="text" name="grade" id="grade" className="w-100" placeholder={this.state.grade} onChange = {this.handleChange} />
+            <input type="text" name="grade" id="grade" className="w-100" value={this.state.grade} onChange = {this.handleChange} />
           </div>
         </div>
         <div className="form-group row align-items-end buttons">
